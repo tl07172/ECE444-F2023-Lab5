0df8999746c5630bb2e33410d56e5dad860f4b0a
tl07172 <timlin07172@gmail.com>
2023-10-28 14:15:09 -0400

    Finished everything until Conclusion

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -81,3 +81,28 @@ def test_delete_message(client):
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
+    
+def test_search(client):
+
+    # Create a test in the database
+    with app.app_context():
+        post = models.Post(title="Test Post", content="This is a test post")
+        db.session.add(post)
+        db.session.commit()
+
+    response = client.get('/search/')
+
+    # Check if the rendered template still contains the test entry (since no query)
+    assert b'Test Post' in response.data
+    assert b'This is a test post' in response.data
+    
+    
+def test_delete_message(client):
+    """Ensure the messages are being deleted"""
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 1
